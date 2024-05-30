5b3237c63dee7c46f174e857aa4a70baaf4a1b69
Barłomiej Kleszcz <b_kleszcz@interia.pl>
2024-05-30 19:22:51 +0200

    Rozbicie testów na osobne metody

--- src/test/java/org/futurecollars/lesson2task4ageVeryfication/AgeVeryficationTest.java
+++ src/test/java/org/futurecollars/lesson2task4ageVeryfication/AgeVeryficationTest.java
@@ -6,22 +6,40 @@ import org.junit.jupiter.api.Assertions;
 public class AgeVeryficationTest {
 
     @Test
-    public void shouldVerifyAge(){
+    public void shouldBeLowerAge(){
 //        given
         int ageLower = 16;
-        int ageEqual = 18;
-        int ageHigher = 23;
 
 //        when
         boolean testAgeLower = AgeVeryfication.verifyAge(ageLower);
+//        then
+        Assertions.assertFalse(testAgeLower);
+
+    }
+    @Test
+    public void shouldBeAdultWith18Years(){
+//        given
+        int ageEqual = 18;
+
+//        when
         boolean testAgeEqual = AgeVeryfication.verifyAge(ageEqual);
+
+//        then
+        Assertions.assertTrue(testAgeEqual);
+    }
+
+    @Test
+    public void shouldBeAdult(){
+
+//        given
+        int ageHigher = 23;
+
+//        when
         boolean testAgeHigher = AgeVeryfication.verifyAge(ageHigher);
 
 //        then
-        Assertions.assertFalse(testAgeLower);
-        Assertions.assertTrue(testAgeEqual);
         Assertions.assertTrue(testAgeHigher);
 
-
     }
+
 }
