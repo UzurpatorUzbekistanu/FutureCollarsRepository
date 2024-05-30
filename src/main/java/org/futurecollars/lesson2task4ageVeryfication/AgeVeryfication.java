381d09762a52180d965a2297c6a86104d6e6a92f
Barłomiej Kleszcz <b_kleszcz@interia.pl>
2024-05-30 19:28:06 +0200

    Uproszczenie metody verifyAge()

--- src/main/java/org/futurecollars/lesson2task4ageVeryfication/AgeVeryfication.java
+++ src/main/java/org/futurecollars/lesson2task4ageVeryfication/AgeVeryfication.java
@@ -3,7 +3,6 @@ package org.futurecollars.lesson2task4ageVeryfication;
 public class AgeVeryfication {
 
     public static boolean verifyAge(int age){
-        if (age >= 18) return true;
-        else return false;
+        return (age >= 18);
     }
 }
